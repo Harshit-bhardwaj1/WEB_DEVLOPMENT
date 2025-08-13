0c13a28bc36d4124b390f3814d7088e81f2a5095
Harshit Bhardwaj <harshitbhardwaj4940@gmail.com>
2025-08-13 16:35:08 +0530

    Add files via upload

---
+++ html college/lecture_14/app.js
@@ -0,0 +1,67 @@
+let abc=10;
+console.log(abc);
+
+
+// nuumbers
+
+// let a=10;
+// let b=10.15;
+// let c=+Infinity;
+// let d=-Infinity;
+// console.log(a);
+// console.log(b);
+// console.log(c);
+// console.log(d);
+// console.log(a,b);
+
+
+// // type
+
+// console.log(typeof(a));
+// console.log(typeof(b));
+
+
+// String --> 3 (3 types to define string)
+
+// let a= 'sam';
+// let b= "vohra";
+// let c = `maverick`; // backticks
+
+// console.log(a, b, c);
+
+// console.log(typeof(a));
+// console.log(typeof(b));
+// console.log(typeof(c));
+
+//  Concatenation
+
+let naam = "samarth";
+let kaam= "time pass";
+let state= "new Delhi";
+
+
+// conacate (we do not use this)
+
+let out = "hi i am " + naam + " my job is to do " + kaam+ " and i put up in "+ state;
+
+console.log(out);
+
+// String template literals (use this)
+
+let out2= `hi i am ${naam} my job is to do ${kaam} and i put up in ${state}`;
+
+console.log(out2);
+
+
+// ------------------------------------
+
+// operators
+
+//  follows PEMDAS rule (parenthesis exponential multiplication  division addition subtraction)
+
+let a=5;
+let b= 2;
+
+console.log(a%b);
+
+console.log(a/b);  // 2.5 (because in this is everything is number)
